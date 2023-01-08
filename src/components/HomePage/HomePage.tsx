670d8fec2554620cbe041a59a4868b7728d1da19
shinyfuwante <mr.philliptang@gmail.com>
2023-01-07 22:42:48 -0800

    Add basic navbar component

--- src/components/HomePage/HomePage.tsx
+++ src/components/HomePage/HomePage.tsx
@@ -1,7 +1,8 @@
-import React from 'react'
+import React from 'react';
+import Navbar from '../Navbar/Navbar';
 
 export default function Homepage() {
   return (
-    <div>Homepage</div>
+    <Navbar></Navbar> 
   )
 }
---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <div className = "flex justify-between p-4">
+        <div>Homepage</div>
+        <div>Storefront</div>
+        <div>Cart</div>
+    </div>
+  )
+}
