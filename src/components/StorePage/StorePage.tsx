9d19c6dd03816cd9d3befeced1a4ec15207e285f
shinyfuwante <mr.philliptang@gmail.com>
2023-01-13 02:23:32 -0800

    Get some sort of component from data from API

--- src/components/StorePage/StorePage.tsx
+++ src/components/StorePage/StorePage.tsx
@@ -1,7 +1,35 @@
-import React from 'react'
+import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
 
+// define interface of Product 
+
+interface Product {
+    id: number;
+    title: string;
+    price: number;
+    image: string;
+}
 export default function StorePage() {
+    // declare products as an array of Product[]
+  const [products, setProducts] = useState<Product[]>([]);
+  const getProducts = async (amount: number) => {
+    const response: Response = await fetch(
+      `https://fakestoreapi.com/products?limit=${amount}`
+    );
+    const data = await response.json();
+    // set the products array to the return value
+    setProducts(data);
+  };
+
+  useEffect(() => {
+    getProducts(5);
+  }, []);
   return (
-    <div>StorePage</div>
-  )
+    <>
+      {products.map(product => {
+       return <div key = {product.id}>{product.title}</div>
+    })}
+    </>
+  );
 }
