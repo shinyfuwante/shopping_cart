6389d7188e828de1840773f22c3260a7c8cafc44
shinyfuwante <mr.philliptang@gmail.com>
2023-01-18 21:57:51 -0800

    CartItem html

--- src/components/CartItem/CartItem.tsx
+++ src/components/CartItem/CartItem.tsx
@@ -8,8 +8,11 @@ const CartItem = ({ product }: CartItemProps) => {
     <div className="font-quicksand w-90 m-1 bg-white border-4 border-solid border-indigo-700">
       <div className="grid grid-cols-2 h-90">
         <img className="h-40 justify-self-center" src={product.product.image} alt={product.product.title} />
-        <div>
-          Stuff
+        <div className="flex flex-col">
+          <div className="truncate">{product.product.title}</div>
+          <div>${product.product.price.toFixed(2)}</div>
+          <div>Count: {product.quantity}</div>
+          <div></div>
         </div>
       </div>
     </div>
