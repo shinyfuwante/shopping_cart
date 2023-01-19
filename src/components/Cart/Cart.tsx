7a465d174b2f9d78726aef8d36c20cf726f55e34
shinyfuwante <mr.philliptang@gmail.com>
2023-01-18 20:57:47 -0800

    Add cartItem cards

--- src/components/Cart/Cart.tsx
+++ src/components/Cart/Cart.tsx
@@ -5,14 +5,19 @@ import CartItem from "../CartItem/CartItem";
 import { cartedProduct } from "../Storefront/Storefront";
 
 export interface CartItemProps {
-    product: cartedProduct;
+  product: cartedProduct;
 }
 const Cart = ({ cart }: cartProps) => {
   return (
-    <div className="font-quicksand fixed inset-y-0 right-0 z-10 h-full w-96 gap-2 bg-indigo-300 p-4">
-      {cart.map((item) => {
-        return <CartItem key={item.product.id} product={item}/>
-      })}
+    <div className="font-quicksand fixed inset-y-0 right-0 z-10 flex h-full w-96 flex-col gap-2 bg-indigo-300">
+      <div className="bg-indigo-200 p-4 text-xl font-bold border-b-black border-2"> Your Cart</div>
+      <div className="p-2 overflow-auto">
+        <div>
+          {cart.map((item) => {
+            return <CartItem key={item.product.id} product={item} />;
+          })}
+        </div>
+      </div>
     </div>
   );
 };
--- src/components/CartItem/CartItem.tsx
+++ src/components/CartItem/CartItem.tsx
@@ -3,10 +3,15 @@ import { Product } from "../Storefront/Storefront";
 import { cartProps } from "../Storefront/Storefront";
 import { CartItemProps } from "../Cart/Cart";
 
-const CartItem = ({product}: CartItemProps) => {
+const CartItem = ({ product }: CartItemProps) => {
   return (
-    <div className="font-quicksand w-90 h-auto">
-      CartItem
+    <div className="font-quicksand w-90 m-1 bg-white border-4 border-solid border-indigo-700">
+      <div className="grid grid-cols-2 h-90">
+        <img className="h-40 justify-self-center" src={product.product.image} alt={product.product.title} />
+        <div>
+          Stuff
+        </div>
+      </div>
     </div>
   );
 };
