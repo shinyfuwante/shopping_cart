96b8d401294ab260403ec2881ff86e2fd2addea0
shinyfuwante <mr.philliptang@gmail.com>
2023-01-19 22:54:07 -0800

    Try browserrouter wrap

--- src/components/Storefront/Storefront.tsx
+++ src/components/Storefront/Storefront.tsx
@@ -7,20 +7,20 @@ import { useState, MouseEventHandler } from "react";
 import routes from "../routes.json";
 
 export interface NavbarProps {
-  handleCartClick: MouseEventHandler
+  handleCartClick: MouseEventHandler;
 }
 export interface cartProps {
   cart: Array<cartedProduct>;
-  handleAddCart: (params: any) => void
-  handleSubtractCart: (params: any) => void
+  handleAddCart: (params: any) => void;
+  handleSubtractCart: (params: any) => void;
 }
 export interface StorePageProps {
-  handleAddCart: (params: any) => void
-  handleSubtractCart: (params: any) => void
+  handleAddCart: (params: any) => void;
+  handleSubtractCart: (params: any) => void;
 }
 export interface cartModify {
-  handleAddCart: (params: any) => void
-  handleSubtractCart: (params: any) => void
+  handleAddCart: (params: any) => void;
+  handleSubtractCart: (params: any) => void;
 }
 export interface Product {
   id: number;
@@ -30,14 +30,16 @@ export interface Product {
 }
 export interface cartedProduct {
   product: Product;
-  quantity: number
+  quantity: number;
 }
 const Storefront = () => {
   const [displayCart, setDisplayCart] = useState(false);
   const [cart, setCart] = useState<cartedProduct[]>([]);
   const addToCart = (product: Product, amount = 1) => {
-    const productToAdd = {product: product, quantity: amount}
-    const oldProduct = cart.findIndex(cartedItem => cartedItem.product === product)
+    const productToAdd = { product: product, quantity: amount };
+    const oldProduct = cart.findIndex(
+      (cartedItem) => cartedItem.product === product
+    );
     if (oldProduct === -1) {
       setCart([...cart, productToAdd]);
     } else {
@@ -46,27 +48,44 @@ const Storefront = () => {
       productToUpdate.quantity += amount;
       setCart(newCart);
     }
-  }
+  };
   const subtractFromCart = (product: Product, amount = 1) => {
-    const oldProduct = cart.findIndex(cartedItem => cartedItem.product === product)
+    const oldProduct = cart.findIndex(
+      (cartedItem) => cartedItem.product === product
+    );
     if (oldProduct > -1) {
       let newCart = [...cart];
       const productToUpdate = newCart[oldProduct];
       productToUpdate.quantity -= amount;
       if (productToUpdate.quantity === 0) {
-        newCart = newCart.filter(product => product !== productToUpdate);
+        newCart = newCart.filter((product) => product !== productToUpdate);
       }
       setCart(newCart);
     }
-  }
+  };
   return (
     <>
-      <Navbar handleCartClick={()=>setDisplayCart(!displayCart)}></Navbar>
-      {displayCart && <Cart cart={cart} handleAddCart={addToCart} handleSubtractCart={subtractFromCart}></Cart>}
       <BrowserRouter basename={routes.BASE}>
+        <Navbar handleCartClick={() => setDisplayCart(!displayCart)}></Navbar>
+        {displayCart && (
+          <Cart
+            cart={cart}
+            handleAddCart={addToCart}
+            handleSubtractCart={subtractFromCart}
+          ></Cart>
+        )}
+
         <Routes>
           <Route path={routes.HOME} element={<HomePage></HomePage>} />
-          <Route path={routes.STORE} element={<StorePage handleAddCart={addToCart} handleSubtractCart={subtractFromCart}></StorePage>} />
+          <Route
+            path={routes.STORE}
+            element={
+              <StorePage
+                handleAddCart={addToCart}
+                handleSubtractCart={subtractFromCart}
+              ></StorePage>
+            }
+          />
         </Routes>
       </BrowserRouter>
     </>
